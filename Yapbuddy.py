65b677da7a58e2583c0f069927c6b8046a542428
FlamePYB <machrouhmohamed23@outlook.com>
2025-10-06 20:30:47 +0100

    logged opening and closing the app

--- Yapbuddy.py
+++ Yapbuddy.py
@@ -1,9 +1,12 @@
 from src.main import main
 import sys
-
+import logging as lg
+lg.basicConfig(level="DEBUG",format="[%(levelname)s] %(message)s")
 def run():
-    return main()
-
+    lg.info("YapBuddy started")
+    running = main()
+    lg.info("YapBuddy closed")
+    return running
 
 if __name__ == "__main__":
     sys.exit(run())
